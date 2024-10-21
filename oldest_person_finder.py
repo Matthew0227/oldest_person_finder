394c84083a250d47c8bd00ac8cc08336c4b2153a
Matthew0227 <matthewjaveniar2@gmail.com>
2024-10-21 12:04:54 +0800

    done with the loop and storing values in list in progress

---
+++ oldest_person_finder.py
@@ -0,0 +1,10 @@
+#Create a program that ask user to input name and age. 
+# Print error message when the input is not valid. 
+# Set your own definition of valid name and valid age. 
+# Store all the collected information into array. 
+# After every input, ask the user if want to input another entry. 
+# When “Yes”, ask the user again for input. Do it until the user respond “No”. 
+# When the user responded “No”, display the name and age of the oldest person.  
+# Use the array in checking who is the oldest
+
+#while loop with try-except block and if statement to ask the user to input a name 
