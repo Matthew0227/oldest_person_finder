81f6f48f4110122af55c682c04531884d8937568
Matthew0227 <matthewjaveniar2@gmail.com>
2024-10-22 00:02:42 +0800

    done fixing the code and changing dictionary into list to use sort method

--- oldest_person_finder.py
+++ oldest_person_finder.py
@@ -1,18 +1,8 @@
-# Create a program that ask user to input name and age. 
-# Print error message when the input is not valid. 
-# Set your own definition of valid name and valid age. 
-# Store all the collected information into array. 
-# After every input, ask the user if want to input another entry. 
-# When “Yes”, ask the user again for input. Do it until the user respond “No”. 
-# When the user responded “No”, display the name and age of the oldest person.  
-# Use the array in checking who is the oldest
-
-# variables
-persons_list = {}
-new_person_info = {}
+persons_info = []
 
 # function for name
 def input_name(ask_user_for_name):
+    #loop 1: asked the user for a name and ensures that the users inputs a letters only value
     while True:
         name = input(ask_user_for_name)
         if name.isalpha():
@@ -22,33 +12,41 @@ def input_name(ask_user_for_name):
 
 # function for number
 def input_age(ask_user_for_number):
+    #loop 2: asked the user for a number and ensures that the users inputs a letters only value
     while True:
         age = input(ask_user_for_number)
         if age.isdigit():
             age = int(age)
             if 0 < age <= 120:
-                print("rerer")
                 return age
+            else:
+                print("Please enter a number between 1-120 for your age: ")
         else:
             print("Please enter a number between 1-120 for your age: ")
 
-name = input_name("Please enter a name: ")
-age = input_age("Please enter an age: ")
-
+# function that specify the sort by age
+def my_func(e):
+    return e["age"]
 
 # Loop 1: ask the user to enter value
 while True:
-    new_person_info[age] = {
-        "name" : name,
-        "age" : age
-    }
+
+    name = input_name("Please enter a name: ")
+    age = input_age("Please enter an age: ")
+
+    persons_info.append({"name": name, "age": age})
 
     try_again = input("enter another one? (y/n): ")
     try_again = try_again.lower()
     
     if try_again == "n":
-            # ends the loop 1 if the user enters the letter n
-        print(new_person_info)
         break
 
+#sorts the persons list by age
+persons_info.sort(key=my_func, reverse=True)
+
+oldest_person = persons_info[0]
+
+
+print(f"The oldest is {oldest_person['name']} who is {oldest_person['age']} years old.")
         
